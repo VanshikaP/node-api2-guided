baaf554d6a0ead28efdf5e981b526a6121328133
VanshikaP <vanshikapundir92@gmail.com>
2020-04-07 23:17:45 -0700

    lecture

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const hubsRouter = require('./hubs/hubs-router.js')
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/hubs', hubsRouter)
+server.use('/junk', hubsRouter)
+// endpoints
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Lambda Hubs API</h>
+      <p>Welcome to the Lambda Hubs API</p>
+    `);
+  });
+
+
+module.exports = server;
+
+
